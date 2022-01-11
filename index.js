afc108182efaa1168137e2573f3bb711439f467c
yusuke <ID+username@users.noreply.github.com>
2022-01-10 22:52:22 -0800

    fix tiny things

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
-const express =  require('express');
+const express = require('express');
 const app = express();
 
-if (process.env.NODE_ENV == 'development') { 
-  require('dotenv').config({path:'.env.dev'});
-}else { 
-  require('dotenv').config({path:'.env.prod'})
+if (process.env.NODE_ENV == 'development') {
+  require('dotenv').config({ path: '.env.dev' });
+} else {
+  require('dotenv').config({ path: '.env.prod' })
 }
 
 const data = process.env.SAMPLE_TEXT;
 
-app.get('/' , (req,res) => { 
+app.get('/', (req, res) => {
   res.send(data);
 });
 
+app.get('/task-list', (req, res) => {
+  res.send(data)
+});
+
+app.get('/task-list/:id', (req, res) => {
+  res.send(req.params.id + data);
+});
+
+
 const PORT = process.env.PORT || '5000';
 
-app.listen(PORT, () => { 
+app.listen(PORT, () => {
   console.log('server listening at 5000')
 });
