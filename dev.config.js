59e03079fbec328b122b79a9fdc9b01f01d5b97a
yusuke <ID+username@users.noreply.github.com>
2022-01-10 22:52:03 -0800

    config file for dev and prod app

---
+++ dev.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  apps: [{
+    name: 'app for dev',
+    script: 'index.js',
+    env: {
+      "PORT": 5000,
+      'NODE_ENV': 'development'
+    },
+    watch: true
+  }],
+  deploy: {
+    production: {
+      user: 'SSH_USERNAME',
+      host: 'SSH_HOSTMACHINE',
+      ref: 'origin/master',
+      repo: 'GIT_REPOSITORY',
+      path: 'DESTINATION_PATH',
+      'pre-deploy-local': '',
+      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': ''
+    }
+  }
+};
---
+++ prod.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  apps: [{
+    name: 'app for prod',
+    script: 'index.js',
+    env: {
+      "PORT": 5001,
+      "NODE_ENV": "production"
+    },
+    watch: false,
+  }],
+  deploy: {
+    production: {
+      user: 'SSH_USERNAME',
+      host: 'SSH_HOSTMACHINE',
+      ref: 'origin/master',
+      repo: 'GIT_REPOSITORY',
+      path: 'DESTINATION_PATH',
+      'pre-deploy-local': '',
+      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production',
+      'pre-setup': ''
+    }
+  }
+};
