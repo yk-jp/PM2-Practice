7ceb86c499e160dd2dc0ac1e00230a93fd14a25c
yusuke <ID+username@users.noreply.github.com>
2022-01-10 23:20:28 -0800

    fix config file

--- prod.config.js
+++ prod.config.js
@@ -16,7 +16,7 @@ module.exports = {
       repo: 'GIT_REPOSITORY',
       path: 'DESTINATION_PATH',
       'pre-deploy-local': '',
-      'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production',
+      'post-deploy': 'npm install && pm2 reload prod.config.js --env production',
       'pre-setup': ''
     }
   }
